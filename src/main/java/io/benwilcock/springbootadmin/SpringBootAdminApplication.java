b82cbb1bf2fe29777dd7ebf368e019d6fca96a1e
benwilcock <benwilcock@gmail.com>
2022-03-29 14:57:34 +0100

    trying Kubernetes discovery client

--- src/main/java/io/benwilcock/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/io/benwilcock/springbootadmin/SpringBootAdminApplication.java
@@ -2,7 +2,9 @@ package io.benwilcock.springbootadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
@@ -10,6 +12,8 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
@@ -24,5 +28,4 @@ public class SpringBootAdminApplication {
 				.and().csrf().disable();
 		}
 	}
-
 }
